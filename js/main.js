0e592a0e9c6660049bbaf6d8022555f8b565ee29
essymo <esthermochari@gmail.com>
2014-02-18 17:00:51 +0300

    on click

---
+++ js/main.js
@@ -0,0 +1,31 @@
+ 
+alert("Hello ESTHER!");
+/*var message =  false
+  if (message === undefined)
+    {
+    alert("True!" + message)
+    }
+    else {  
+    alert("False" + message)
+    }
+var button = document.getElementById ('my button')
+  console.log ("button is:" + button)
+  button.addEventListener('click, funtion(e)'){
+    alert("button was clicked")
+});*/
+$('#my-button').on("click", function(){alert("You clicked")})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
